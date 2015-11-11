8df3e4fea7dd10c5e988117e4ba0862d4df1631d
efrederic <ethan.frederic@gmail.com>
2015-11-10 18:49:54 -0600

    added bluetooth capabilities

--- app/src/main/java/com/rev/xi/rev_gps/MapsActivity.java
+++ app/src/main/java/com/rev/xi/rev_gps/MapsActivity.java
@@ -1,6 +1,11 @@
 package com.rev.xi.rev_gps;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
 import android.location.Location;
+import android.os.Handler;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
@@ -17,20 +22,31 @@ import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
 import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.text.DateFormat;
 import java.util.Date;
+import java.util.Set;
+import java.util.UUID;
 
 public class MapsActivity extends FragmentActivity implements ConnectionCallbacks, OnConnectionFailedListener, LocationListener {
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
-    private String TAG = "MapActivity";
+    private String TAG = "REV_GPS.MapActivity";
     private Double longitude;
     private Double latitude;
     LocationRequest mLocationRequest;
     private final static int PLAY_SERVICES_RESOLUTION_REQUEST = 1000;
+
+    private OutputStream bluetoothOutput;
+    private InputStream bluetoothInput;
+    private boolean stopWorker = false;
 
     @Override
     public void onConnectionSuspended(int i) {
@@ -61,6 +77,13 @@ public class MapsActivity extends FragmentActivity implements ConnectionCallback
             mLocationRequest.setFastestInterval(1000);
             mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
         }
+        Button connectBluetoothButton = (Button) findViewById(R.id.bluetooth_connect);
+        connectBluetoothButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                connectArduinoBluetooth();
+            }
+        });
     }
 
     @Override
@@ -92,8 +115,8 @@ public class MapsActivity extends FragmentActivity implements ConnectionCallback
         ((TextView) findViewById(R.id.latitude)).setText("latitude: " + latitude.toString());
         ((TextView) findViewById(R.id.longitude)).setText("longitude: " + longitude.toString());
         ((TextView) findViewById(R.id.time)).setText("time: " + time);
-        ((TextView) findViewById(R.id.speed)).setText("speed: " + mLastLocation.getSpeed());
-        ((TextView) findViewById(R.id.accuracy)).setText("accuracy: " + mLastLocation.getAccuracy());
+        ((TextView) findViewById(R.id.speed)).setText("speed: " + mLastLocation.getSpeed() + " m/s");
+        ((TextView) findViewById(R.id.accuracy)).setText("accuracy: " + mLastLocation.getAccuracy() + " m");
     }
 
     @Override
@@ -140,8 +163,6 @@ public class MapsActivity extends FragmentActivity implements ConnectionCallback
         mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
     }
 
-
-
     @Override
     public void onLocationChanged(Location location) {
         mLastLocation = location;
@@ -164,5 +185,79 @@ public class MapsActivity extends FragmentActivity implements ConnectionCallback
             return false;
         }
         return true;
+    }
+
+    private void connectArduinoBluetooth() {
+        BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();
+        if(!adapter.isEnabled()) {
+            Intent enableBluetooth = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBluetooth, 0);
+        }
+        Set<BluetoothDevice> pairedDevices = adapter.getBondedDevices();
+        BluetoothDevice pairedDevice = null;
+        if(pairedDevices.size() > 0) {
+            for(BluetoothDevice device : pairedDevices) {
+                if(device.getName().equals("NAME OF BLUETOOTH DEVICE HERE")) { //TODO: input arduino name
+                    pairedDevice = device;
+                    break;
+                }
+            }
+        }
+        if (pairedDevice == null) {
+            ((TextView) findViewById(R.id.arduino_input)).setText("failed to find paired device");
+            return;
+        }
+        UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb"); //Standard SerialPortService ID
+        try {
+            BluetoothSocket socket = pairedDevice.createRfcommSocketToServiceRecord(uuid);
+            socket.connect();
+            bluetoothOutput = socket.getOutputStream();
+            bluetoothInput = socket.getInputStream();
+            listenForData();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void listenForData() {
+        final Handler handler = new Handler();
+        Thread workerThread = new Thread(new Runnable() {
+            public void run() {
+                byte[] readBuffer = new byte[1024];
+                int readBufferPosition = 0;
+                while(!Thread.currentThread().isInterrupted() && !stopWorker) {
+                    int bytesAvailable = 0;
+                    try {
+                        bytesAvailable = bluetoothInput.available();
+                        if(bytesAvailable > 0) {
+                            byte[] packetBytes = new byte[bytesAvailable];
+                            bluetoothInput.read(packetBytes);
+
+                            for(int i = 0; i < bytesAvailable; i++) {
+                                byte b = packetBytes[i];
+                                if(b == '\n') {  // delimiter character to be sent at the end of every arduino command
+                                    byte[] encodedBytes = new byte[readBufferPosition];
+                                    System.arraycopy(readBuffer, 0, encodedBytes, 0, encodedBytes.length);
+                                    final String data = new String(encodedBytes, "US-ASCII");
+                                    readBufferPosition = 0;
+                                    // The variable data now contains our full command
+                                    // handler used to display data back on UI thread
+                                    handler.post(new Runnable() {
+                                        public void run() {
+                                            ((TextView) findViewById(R.id.arduino_input)).setText(data);
+                                        }
+                                    });
+                                } else {
+                                    readBuffer[readBufferPosition++] = b;
+                                }
+                            }
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+        workerThread.start();
     }
 }
